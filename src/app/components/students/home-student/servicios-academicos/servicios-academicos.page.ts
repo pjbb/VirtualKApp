1340c1ae15fd3b1d29aa9c26a0cf6c9c0f9cb584
pjbb <paulaj.benitezb@gmail.com>
2019-08-13 19:48:28 -0500

    New Page Servicios Academicos
    Add new page to University academics services, and new list

---
+++ src/app/components/students/home-student/servicios-academicos/servicios-academicos.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servicios-academicos',
+  templateUrl: './servicios-academicos.page.html',
+  styleUrls: ['./servicios-academicos.page.scss'],
+})
+export class ServiciosAcademicosPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
